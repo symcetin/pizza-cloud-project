1c5d1bdea9e83bb327758df5e00e033b18b5b4df
symcetin <seyma.oskurcu@gmail.com>
2022-10-08 21:14:54 -0400

    Initial commit

---
+++ src/main/java/com/cydeo/pizzacloud/repository/PizzaRepository.java
@@ -0,0 +1,23 @@
+package com.cydeo.pizzacloud.repository;
+
+import com.cydeo.pizzacloud.model.Pizza;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PizzaRepository {
+
+    private static List<Pizza> pizzaList = new ArrayList<>();
+
+    public Pizza createPizza(Pizza pizzaToSave) {
+        pizzaList.add(pizzaToSave);
+        return pizzaToSave;
+    }
+
+    public List<Pizza> readAll() {
+        return pizzaList;
+    }
+
+}
